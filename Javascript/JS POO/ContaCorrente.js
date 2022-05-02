ff53bdd1aa930b5a58ffc1c0e33b9518edb4376c
RenanCruz7 <renancruz199@gmail.com>
2022-05-01 22:35:37 -0300

    conserto no saldo da conta

--- Javascript/JS POO/ContaCorrente.js
+++ Javascript/JS POO/ContaCorrente.js
@@ -1,15 +1,21 @@
 export class ContaCorrente{
     agencia
-    saldo
+    cliente
+    _saldo = 0
 
     sacar(valor){
-        this.saldo -=valor
+        this._saldo -=valor
         return valor;
     }
 
     depositar(valor){
         if(valor >0){
-            this.saldo+=valor
+            this._saldo+=valor
         }
+    }
+
+    transferir (valor,conta){
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado)
     }
 }
--- Javascript/JS POO/index.js
+++ Javascript/JS POO/index.js
@@ -6,35 +6,25 @@ const contaCorrenteRenan = new ContaCorrente()
 
 cliente1.nome = "Renan"
 cliente1.cpf = 50924618884
+contaCorrenteRenan.cliente = cliente1
 contaCorrenteRenan.agencia = 10
-contaCorrenteRenan.saldo = 1000
-
+contaCorrenteRenan.depositar(500)
+/** ------------------------------------------------------------------------------------------- */
 const cliente2 = new Cliente()
 const contaCorrenteMatheus = new ContaCorrente()
-
 cliente2.nome = "Matheus"
 cliente2.cpf = 77788866688
 contaCorrenteMatheus.agencia = 10
-contaCorrenteMatheus.saldo = 1000
+/** ------------------------------------------------------------------------------------------- */
+const conta2 = new ContaCorrente()
+conta2.cliente = cliente2
+conta2.agencia = 102
+/** ------------------------------------------------------------------------------------------- */
 
-const valorSacado =contaCorrenteRenan.sacar(1000)
-console.log(valorSacado)
 console.log(contaCorrenteRenan)
+console.log(conta2)
 
-/**
-console.log(cliente1)
-console.log(contaCorrenteRenan)
-
-console.log("                        ")
-
-console.log(cliente2)
-console.log(contaCorrenteMatheus)
-
-console.log("                        ")
-console.log('sua conta ficou com o seguinte saldo após o saque: ',contaCorrenteRenan.saldo)
-
-contaCorrenteMatheus.depositar(-500)
-console.log(contaCorrenteMatheus)
-*/
+contaCorrenteRenan.transferir(200, conta2)
+console.log(conta2)
 
 

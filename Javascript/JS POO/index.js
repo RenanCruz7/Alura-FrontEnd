b0285e087136a4271a4d7c154efbc3670336882c
RenanCruz7 <renancruz199@gmail.com>
2022-05-03 13:30:27 -0300

    curso finalizado

--- Javascript/JS POO/index.js
+++ Javascript/JS POO/index.js
@@ -1,30 +1,16 @@
 import {Cliente} from "./cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente()
-const contaCorrenteRenan = new ContaCorrente()
+const cliente1 = new Cliente("Renan",50924618884)
+const contaCorrenteRenan = new ContaCorrente(cliente1,1001)
 
-cliente1.nome = "Renan"
-cliente1.cpf = 50924618884
-contaCorrenteRenan.cliente = cliente1
-contaCorrenteRenan.agencia = 10
-contaCorrenteRenan.depositar(500)
-/** ------------------------------------------------------------------------------------------- */
-const cliente2 = new Cliente()
-const contaCorrenteMatheus = new ContaCorrente()
-cliente2.nome = "Matheus"
-cliente2.cpf = 77788866688
-contaCorrenteMatheus.agencia = 10
-/** ------------------------------------------------------------------------------------------- */
-const conta2 = new ContaCorrente()
-conta2.cliente = cliente2
-conta2.agencia = 102
-/** ------------------------------------------------------------------------------------------- */
+const cliente2 = new Cliente("Matheus",77788866688)
+const contaCorrenteMatheus = new ContaCorrente(cliente2,1002)
 
 console.log(contaCorrenteRenan)
-console.log(conta2)
+console.log(contaCorrenteMatheus)
+console.log(ContaCorrente.numeroContas)
 
-contaCorrenteRenan.transferir(200, conta2)
-console.log(conta2)
+
 
 

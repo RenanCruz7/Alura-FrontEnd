7ff46d1924ed559edd8c15bbb19c3bd4f388ed44
RenanCruz7 <renancruz199@gmail.com>
2022-05-31 17:21:32 -0300

    autenticação cliente

--- Javascript/JS POO/cliente.js
+++ Javascript/JS POO/cliente.js
@@ -1,13 +1,17 @@
-export class Cliente{
-    /** Get = pegar portanto nao vou poder alterar o valor do CPF quando ele for passado 
+export class Cliente {
+  /** Get = pegar portanto nao vou poder alterar o valor do CPF quando ele for passado 
      pela primeira vez
     */
-    get cpf(){
-        return this._cpf
-    }
-    //O construtor é um método especial para criar e inicializar um objeto criado a partir de uma classe.
-    constructor(nome,cpf){
-        this.nome = nome
-        this._cpf = cpf
-    }
+  get cpf() {
+    return this._cpf;
+  }
+  //O construtor é um método especial para criar e inicializar um objeto criado a partir de uma classe.
+  constructor(nome, cpf, senha) {
+    this.nome = nome;
+    this._cpf = cpf;
+    this._senha = senha;
+  }
+  autenticar() {
+    return true;
+  }
 }
--- Javascript/JS POO/index.js
+++ Javascript/JS POO/index.js
@@ -1,3 +1,4 @@
+import { Cliente } from './cliente.js';
 import { Gerente } from './funcionario/Gerente.js';
 import { Diretor } from './funcionario/Diretor.js';
 import { SistemaAutenticacao } from './sistemaAutenticacao.js';
@@ -7,7 +8,11 @@ diretor.cadastrarSenha('123456');
 const gerente = new Gerente('Renan', 5000, 12345678911);
 gerente.cadastrarSenha('180607');
 
-const estaLogado = SistemaAutenticacao.login(gerente, '180607');
-console.log(estaLogado);
-const estaLogado2 = SistemaAutenticacao.login(diretor, '123456');
-console.log(estaLogado2);
+const GerenteEstaLogado = SistemaAutenticacao.login(gerente, '180607');
+console.log(GerenteEstaLogado);
+const DiretorEstaLogado = SistemaAutenticacao.login(diretor, '123456');
+console.log(DiretorEstaLogado);
+
+const cliente = new Cliente('Lais', 12345678900, 78910);
+const ClienteEstaLogado = SistemaAutenticacao.login(cliente, '78910');
+console.log(ClienteEstaLogado);

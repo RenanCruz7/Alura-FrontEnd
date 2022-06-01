ca3e2fe31df68fe9ae4f9cac155af9174c0e03c8
RenanCruz7 <renancruz199@gmail.com>
2022-06-01 09:33:51 -0300

    Fim do Curso

--- Javascript/JS POO/index.js
+++ Javascript/JS POO/index.js
@@ -1,7 +1,7 @@
-import { Cliente } from './cliente.js';
 import { Gerente } from './funcionario/Gerente.js';
 import { Diretor } from './funcionario/Diretor.js';
 import { SistemaAutenticacao } from './sistemaAutenticacao.js';
+import { Cliente } from './cliente.js';
 
 const diretor = new Diretor('Matheus', 10000, 12345678900);
 diretor.cadastrarSenha('123456');
@@ -10,9 +10,15 @@ gerente.cadastrarSenha('180607');
 
 const GerenteEstaLogado = SistemaAutenticacao.login(gerente, '180607');
 console.log(GerenteEstaLogado);
+
 const DiretorEstaLogado = SistemaAutenticacao.login(diretor, '123456');
 console.log(DiretorEstaLogado);
 
 const cliente = new Cliente('Lais', 12345678900, 78910);
 const ClienteEstaLogado = SistemaAutenticacao.login(cliente, '78910');
 console.log(ClienteEstaLogado);
+
+/*
+Encapsular os dados de uma aplicação significa evitar que estes sofram acessos indevidos
+Para isso, é criada uma estrutura que contém métodos que podem ser utilizados por qualquer outra classe, sem causar inconsistências no desenvolvimento de um código.
+*/

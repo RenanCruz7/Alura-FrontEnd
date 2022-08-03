d53f95cb51830a20cab0a88b57077886400f9e1b
RenanCruz7 <renancruz199@gmail.com>
2022-08-02 22:38:25 -0300

    metodo get correto

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -5,16 +5,16 @@ class Negociacao {
         this._valor = valor
     }
 
-    getVol(){
+    get volume(){
         return this._quantidade * this._valor
     }
-    getData(){
+    get data(){
         return this._data
     }
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade
     }
-    getValor(){
+    get valor(){
         return this._valor
     }
 }

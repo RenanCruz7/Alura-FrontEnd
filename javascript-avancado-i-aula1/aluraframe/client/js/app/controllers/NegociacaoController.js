3f955002321cdf6af250804d4aa032ddcdfe8d6c
RenanCruz7 <renancruz199@gmail.com>
2022-08-04 20:29:04 -0300

    constructor e date

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,24 @@
 class NegociacaoController{
+
+    constructor(){
+        this._inputData = document.querySelector('#data')
+        this._inputQuantidade = document.querySelector('#quantidade')
+        this._inputValor = document.querySelector('#valor')
+    }
+
     adiciona(event){
         event.preventDefault()
-        alert('chamaaaaaaaaa')
+        
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map(function(item,indice){
+                if(indice == 1){
+                    return item - 1
+                }
+                return item
+            }))
+            console.log(data)
+       
     }
 }

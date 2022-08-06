c4d6960b5d023c3e5873c1044b3ba35e76def4d1
RenanCruz7 <renancruz199@gmail.com>
2022-08-06 00:51:00 -0300

    explicação sobre MVC com o Miwish

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Neste repositorio irei armazenar todos os projetos e anotações sobre desenvolv
 
 - MVC 
 
+M - modelo dos meus dados - Dados que eu vou usar na minha aplicação
+
+V - View - tudo que será exibido para o usuario
+
+C - Controller - onde é realizada as regras de negocio
+
--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,28 +8,17 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault() //previne o comportamento padrao da pagina ao atualizar quando o envio do form é feito
-        
-        /*
-         Neste trecho de codigo abaixo eu faço a transformação da data de string para object assim como a formatação da mesma
 
-         caso tenha duvidas consultar o seguinte curso: Javascript: conhecendo o Browser - Aula 3 completa
-        */
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item,indice)=> item - indice % 2)) // arrow function 
-        
+        let helper = new DateHelper()
+        let data = helper.textoParaData(this._inputData.value)
         
         let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
             this._inputValor.value
         )
+        console.log(negociacao)
 
-        // formata a data para a forma de dd/mm/yyyy como usamos normalmente 
-        let diaMesAno = negociacao.data.getDate()
-        + '/' + (negociacao.data.getMonth() + 1)
-        + '/' + negociacao.data.getFullYear()
-        console.log(diaMesAno)
+        console.log(helper.dataParaTexto(negociacao.data))
     }
 }

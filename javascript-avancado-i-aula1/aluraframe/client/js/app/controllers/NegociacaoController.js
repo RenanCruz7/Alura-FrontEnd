362330fadc205815bc20211e187abc7ae8d3bac1
RenanCruz7 <renancruz199@gmail.com>
2022-08-05 21:24:48 -0300

    arrow function

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -12,13 +12,12 @@ class NegociacaoController{
         let data = new Date(...
             this._inputData.value
             .split('-')
-            .map(function(item,indice){
-                if(indice == 1){
-                    return item - 1
-                }
-                return item
-            }))
-            console.log(data)
-       
+            .map((item,indice)=> item - indice % 2)) // arrow function 
+
+        let negociacao = new negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
     }
 }

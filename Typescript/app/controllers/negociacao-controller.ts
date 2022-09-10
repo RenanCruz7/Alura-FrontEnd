87bd55b79573d572404e289ec3a496399aa5ddf8
RenanCruz7 <renancruz199@gmail.com>
2022-09-10 20:25:31 -0300

    voltando aos estudos de typescript

--- Typescript/app/controllers/negociacao-controller.ts
+++ Typescript/app/controllers/negociacao-controller.ts
@@ -1,8 +1,10 @@
+import { Negociacao } from "../models/negociacao.js"
+
 export class NegociacaoController{
 
-    private inputData
-    private inputQuantidade
-    private inputValor
+    private inputData: HTMLInputElement
+    private inputQuantidade: HTMLInputElement
+    private inputValor: HTMLInputElement
 
     constructor(){
         this.inputData = document.querySelector('#data')
@@ -10,7 +12,36 @@ export class NegociacaoController{
         this.inputValor = document.querySelector('#valor')
     }
 
+    /** 
+    Por enquanto os valores retornados por criaNegociacao sao impressos no console
+    e apos isso é feito a limpeza dos campos de input para que seja inserido os prox valores
+    */
+    adiciona(): void{
+        const negociacao = this.criaNegociacao()
+        console.log(negociacao)
+        this.limparFormulario()
+    }
 
-
-
+    /**
+    Cria uma negociaçao com os valores que sao coletados do input
+    porem antes é feito um tratamento dos tados ja que .value
+    retorna os dados em string e não é o que queremos
+    */
+    criaNegociacao(): Negociacao{
+        const exp = /-/g
+        const date = new Date(this.inputData.value.replace(exp, ','))
+        const quantidade = parseInt(this.inputQuantidade.value)
+        const valor = parseFloat(this.inputValor.value)
+        return new Negociacao(date,quantidade,valor)
+    }
+    /*
+    Limpa o formulario após o envio das informaçoes e coloca o foco no input de data
+    para que o usuario possa continuar cadastrando novas negociacoes
+    */
+    limparFormulario():void{
+        this.inputData.value = ''
+        this.inputQuantidade.value = ''
+        this.inputValor.value = ''
+        this.inputData.focus()
+    }
 }
--- Typescript/dist/js/controllers/negociacao-controller.js
+++ Typescript/dist/js/controllers/negociacao-controller.js
@@ -1,7 +1,39 @@
+import { Negociacao } from "../models/negociacao.js";
 export class NegociacaoController {
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+    }
+    /**
+    Por enquanto os valores retornados por criaNegociacao sao impressos no console
+    e apos isso é feito a limpeza dos campos de input para que seja inserido os prox valores
+    */
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    /**
+    Cria uma negociaçao com os valores que sao coletados do input
+    porem antes é feito um tratamento dos tados ja que .value
+    retorna os dados em string e não é o que queremos
+    */
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    /*
+    Limpa o formulario após o envio das informaçoes e coloca o foco no input de data
+    para que o usuario possa continuar cadastrando novas negociacoes
+    */
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }

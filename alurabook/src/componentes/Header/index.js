e1f197eebb15bf0510d86809fdc8b89a19846d8c
RenanCruz7 <renancruz199@gmail.com>
2022-11-01 12:11:37 -0300

    Styled Components

--- alurabook/src/componentes/Header/index.js
+++ alurabook/src/componentes/Header/index.js
@@ -1,16 +1,22 @@
-import './style.css'
 import Logo from '../Logo'
 import OpcoesHeader from '../OpcoesHeader'
 import IconesHeader from '../IconesHeader'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
 
 
 function Header(){
     return (
-    <header className="App-header">
+    <HeaderContainer>
         <Logo/> {/* nesta linha estou exibindo o componente*/}
         <OpcoesHeader/>
         <IconesHeader/>
-    </header>
+    </HeaderContainer>
     )
 }
 

d544ca34fa4a3e7824e108fbeb734ca5482eef6b
RenanCruz7 <renancruz199@gmail.com>
2023-01-12 22:41:12 -0300

    Lista

--- alura-studies/src/App.tsx
+++ alura-studies/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Formulario from './components/formulario';
+import Lista from './components/lista';
 
 function App() {
   return (
     <div className="App">
         <Formulario></Formulario>
-        <div>teste</div>
+        <Lista></Lista>    
     </div>
   );
 }

2341a1342d9efbb509adaa1a13c0c5d75562c11b
RenanCruz7 <renancruz199@gmail.com>
2023-01-27 20:58:51 -0300

    testando onClick

--- alura-studies/src/components/botao/index.tsx
+++ alura-studies/src/components/botao/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import style from './Botao.module.scss'
 
-class Botao extends React.Component{
+class Botao extends React.Component<{texto: string}>{
     render(): React.ReactNode {
         return(
-            <button className={style.botao}>Enviar</button>
+            <button className={style.botao} onClick={handleClick}>{this.props.texto}</button>
         )
     }
+}
+
+function handleClick(){
+    alert('Teste de Função')
 }
 
 export default Botao;

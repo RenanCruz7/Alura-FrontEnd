d4dae6b04837a80315f041d5642098f446f810c6
RenanCruz7 <renancruz199@gmail.com>
2023-01-31 22:53:49 -0300

    useState

--- alura-studies/src/components/formulario/index.tsx
+++ alura-studies/src/components/formulario/index.tsx
@@ -3,6 +3,10 @@ import Botao from '../botao'
 import style from './Formulario.module.scss'
 
 class Formulario extends React.Component{
+    state = {
+        tarefa:"",
+        tempo: "00:00"
+    }
     render(): React.ReactNode {
         return(
             <form className={style.novaTarefa}>
@@ -10,14 +14,14 @@ class Formulario extends React.Component{
                     <label htmlFor='tarefa'>
                         Adicione um novo estudo
                     </label>
-                    <input type='text' name='tarefa'id='tarefa' placeholder='O que você quer estudar'required></input>
+                    <input value={this.state.tarefa} onChange={evento => this.setState({...this.state, tarefa: evento.target.value})} type='text' name='tarefa'id='tarefa' placeholder='O que você quer estudar'required></input>
                 </div>
 
                 <div className={style.inputContainer}>
                     <label htmlFor='tempo'>
                         Tempo
                     </label>
-                    <input type='time' step='1' name='tempo' id='tempo' min='00:00:00' max='01:30:00' required></input>
+                    <input value={this.state.tempo} onChange={evento =>this.setState({...this.state, tempo: evento.target.value})} type='time' step='1' name='tempo' id='tempo' min='00:00:00' max='01:30:00' required></input>
                 </div>
                 <Botao> Adicionar</Botao>
             </form>

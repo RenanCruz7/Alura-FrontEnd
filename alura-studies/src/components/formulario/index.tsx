3249a6a6f9d9fcbe11ea09e42f2a1fde4fb8d094
RenanCruz7 <renancruz199@gmail.com>
2023-03-01 14:34:12 -0300

    Recuperando dados do Formulario

--- alura-studies/src/components/formulario/index.tsx
+++ alura-studies/src/components/formulario/index.tsx
@@ -7,9 +7,14 @@ class Formulario extends React.Component{
         tarefa:"",
         tempo: "00:00"
     }
+    adicionarTarefa(evento: React.FormEvent){
+        evento.preventDefault()
+        console.log('state', this.state)
+    }
+
     render(): React.ReactNode {
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>
                         Adicione um novo estudo
@@ -21,7 +26,7 @@ class Formulario extends React.Component{
                     <label htmlFor='tempo'>
                         Tempo
                     </label>
-                    <input value={this.state.tempo} onChange={evento =>this.setState({...this.state, tempo: evento.target.value})} type='time' step='1' name='tempo' id='tempo' min='00:00:00' max='01:30:00' required></input>
+                    <input value={this.state.tempo} onChange={evento =>this.setState({...this.state, tempo: evento.target.value})} type='time' step='1' name='tempo' id='tempo' min='00:00:00' max='10:00:00' required></input>
                 </div>
                 <Botao> Adicionar</Botao>
             </form>

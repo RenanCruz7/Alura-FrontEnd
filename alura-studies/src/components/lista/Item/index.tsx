8712f7d9fd29701361329c7e212064fcf1e62b72
RenanCruz7 <renancruz199@gmail.com>
2023-04-05 19:38:28 -0300

    continuando o curso

--- alura-studies/src/components/lista/Item/index.tsx
+++ alura-studies/src/components/lista/Item/index.tsx
@@ -1,8 +1,8 @@
+import { ITarefa } from '../../../types/tarefas'
 import style from '../Lista.module.scss'
 
 
-export default function Item({tarefa, tempo}: {tarefa:string, tempo:string}){
-
+export default function Item({tarefa, tempo,selecionado,completado,id}: ITarefa){
     return(
         <li className={style.item}>
             <h3>{tarefa}</h3>

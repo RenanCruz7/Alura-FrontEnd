d55f306a1b512c5801bfdcddcad5594caf39595a
RenanCruz7 <renancruz199@gmail.com>
2023-05-11 16:57:57 -0300

    Cadastrando e Listando Restaurantes

--- alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,11 +1,30 @@
-import { TextField, Button} from "@mui/material"
-
+import {TextField,Button} from "@mui/material"
+import axios from "axios"
+import {useState} from "react"
 
 const FormularioRestaurante = () => {
+
+    const[nomeRestaurante,setNomeRestaurante] = useState('')
+
+    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) =>{
+        evento.preventDefault()
+        axios.post('http://localhost:8000/api/v2/restaurantes/',{
+            nome: nomeRestaurante
+        })
+            .then(() =>{
+                alert('Restaurante Cadastrado')
+            })
+    }
+
     return(
-        <form>
-            <TextField id="standard-basic" label="Standard" variant="standard" />
-            <Button variant="outlined">Outlined</Button>
+        <form onSubmit={aoSubmeterForm}>
+            <TextField
+                value={nomeRestaurante}
+                onChange={evento => setNomeRestaurante(evento.target.value)}
+                id="standard-basic"
+                label="Nome do Restaurante"
+                variant="standard" />
+            <Button type="submit" variant="outlined">Salvar</Button>
         </form>
     )
 

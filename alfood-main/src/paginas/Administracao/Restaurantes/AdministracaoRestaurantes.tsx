55d371570f05100c2fd9ba045936a37c293329c0
RenanCruz7 <renancruz199@gmail.com>
2023-05-12 08:59:39 -0300

    Editando Restaurantes

--- alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import IRestaurante from "../../../interfaces/IRestaurante"
 import {TableBody,TableRow ,TableCell,TableHead,Table,Paper, TableContainer } from "@mui/material"
 import axios from "axios"
+import { Link } from "react-router-dom"
 
 
 const AdministracaoRestaurantes = () => {
@@ -20,12 +21,16 @@ const AdministracaoRestaurantes = () => {
                 <TableHead>
                     <TableRow>
                         <TableCell>Nome</TableCell>
+                        <TableCell>Editar</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {restaurantes.map((restaurante) => (
                     <TableRow key={restaurante.id}>
                         <TableCell>{restaurante.nome}</TableCell>
+                        <TableCell>
+                            [<Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link>]
+                        </TableCell>
                     </TableRow>
                     ))}
                 </TableBody>

8259127c562424ae3ef7c6d2e950f512c76ef09e
RenanCruz7 <renancruz199@gmail.com>
2023-05-16 07:48:28 -0300

    Pagina de Administração Pronta

--- alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,61 +1,61 @@
-import {TextField,Button,Typography, Box} from "@mui/material"
+import { TextField, Button, Typography, Box} from "@mui/material"
 import axios from "axios"
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import IRestaurante from "../../../interfaces/IRestaurante"
 import http from "../../../http"
-
 
 const FormularioRestaurante = () => {
 
     const parametros = useParams()
 
-    useEffect(()=>{
-        if(parametros.id){
+    useEffect(() => {
+        if (parametros.id) {
             http.get<IRestaurante>(`restaurantes/${parametros.id}/`)
                 .then(resposta => setNomeRestaurante(resposta.data.nome))
         }
-    },[parametros])
+    }, [parametros])
 
-    const[nomeRestaurante,setNomeRestaurante] = useState('')
+    const [nomeRestaurante, setNomeRestaurante] = useState('')
 
-    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) =>{
+    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault()
 
-        if(parametros.id){
-            http.put(`restaurantes/${parametros.id}/`,{
+        if (parametros.id) {
+            http.put(`restaurantes/${parametros.id}/`, {
                 nome: nomeRestaurante
             })
 
-            .then(()=>{
-                alert("Restaurante atualizado com sucesso!!!!!!")
+                .then(() => {
+                    alert("Restaurante atualizado com sucesso!!!!!!")
+                })
+        } else {
+            http.post('restaurantes/', {
+                nome: nomeRestaurante
             })
-        }else{
-            http.post('restaurantes/',{
-            nome: nomeRestaurante
-            })
-            .then(() =>{
-                alert('Restaurante Cadastrado')
-            })
-        }   
+                .then(() => {
+                    alert('Restaurante Cadastrado')
+                })
+        }
     }
-    return(
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Typography component='h1' variant='h6'>Formulário de Restaurantes</Typography>
-            <Box component='form' onSubmit={aoSubmeterForm}>
-                <TextField
-                    value={nomeRestaurante}
-                    onChange={evento => setNomeRestaurante(evento.target.value)}
-                    id="standard-basic"
-                    label="Nome do Restaurante"
-                    variant="standard"
-                    fullWidth
-                    required
+    return (
+        <>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1 }}>
+                <Typography component='h1' variant='h6'>Formulário de Restaurantes</Typography>
+                <Box component='form' sx={{ width: '100%' }} onSubmit={aoSubmeterForm}>
+                    <TextField
+                        value={nomeRestaurante}
+                        onChange={evento => setNomeRestaurante(evento.target.value)}
+                        id="standard-basic"
+                        label="Nome do Restaurante"
+                        variant="standard"
+                        fullWidth
+                        required
                     />
-                <Button sx={{marginTop:1}} fullWidth type="submit" variant="outlined">Salvar</Button>
+                    <Button sx={{ marginTop: 1 }} fullWidth type="submit" variant="outlined">Salvar</Button>
+                </Box>
             </Box>
-        </Box>
-        
+        </>
     )
 }
 

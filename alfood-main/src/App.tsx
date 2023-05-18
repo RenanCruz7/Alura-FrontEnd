ea5aa26bec025719239afc9851867ea453b19f8b
RenanCruz7 <renancruz199@gmail.com>
2023-05-18 08:10:50 -0300

    Formulario de Pratos e Ajuste em Pagina ADMIN

--- alfood-main/src/App.tsx
+++ alfood-main/src/App.tsx
@@ -4,6 +4,8 @@ import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
+import AdministracaoPratos from './paginas/Pratos/AdministracaoPratos';
+import FormularioPrato from './paginas/Pratos/FormularioPrato';
 
 function App() {
 
@@ -17,7 +19,9 @@ function App() {
         <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
         <Route path="/admin/restaurantes/novo" element={<FormularioRestaurante />} />
         <Route path="/admin/restaurantes/:id" element={<FormularioRestaurante />} />
-        
+
+        <Route path='pratos' element={<AdministracaoPratos/>}></Route>
+        <Route path='pratos/novo' element={<FormularioPrato/>}></Route>
       </Route>
     </Routes>
   );
